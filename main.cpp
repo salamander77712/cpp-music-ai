2f3fd3c1afe900086f203bd9f87bb09e208c0e5d
salamander77712 <56451516+salamander77712@users.noreply.github.com>
2020-03-14 13:29:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 #include "musicai.h"
 #include "musicai.cpp"
 #include <time.h>
+#include <Windows.h>
+#include <MMSystem.h>
+#pragma comment(lib, "winmm.lib")
 int main(){
 	Music m;
 	m.initNotes();
+	m.playNote(0);
 	std::string input = "";
 	srand(time(0));
 	while (true){
 		std::vector<int> output = m.makeSong();
 		for (int i = 0; i < 100; i++) {
-			std::cout << output[i] << '\n';
+			m.playNote(output[i]);
 		}
 		std::cout << "\ngood? (y/n)\n";
 		std::cin >> input;

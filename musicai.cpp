1e49cc04d011bfd638c9bc8d16bd1dc8770bd400
salamander77712 <56451516+salamander77712@users.noreply.github.com>
2020-03-14 13:30:51 -0500

    Update musicai.cpp

--- musicai.cpp
+++ musicai.cpp
@@ -1,5 +1,8 @@
 #include "musicai.h"
 #include <iostream>
+#include <Windows.h>
+#include <MMSystem.h>
+#pragma comment(lib, "winmm.lib")
 Music::Music(){}
 Music::~Music() {
 }
@@ -48,4 +51,114 @@ std::vector<int> Music::makeSong() {
 	}
 	music.push_back(output);
 	return(output);
+}
+void Music::playNote(int note) {
+	if (note == 0) {
+		PlaySound(TEXT("note1.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 1) {
+		PlaySound(TEXT("note2.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 2) {
+		PlaySound(TEXT("note3.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 3) {
+		PlaySound(TEXT("note4.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 4) {
+		PlaySound(TEXT("note5.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 5) {
+		PlaySound(TEXT("note6.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 6) {
+		PlaySound(TEXT("note7.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 7) {
+		PlaySound(TEXT("note8.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 8) {
+		PlaySound(TEXT("note9.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 9) {
+		PlaySound(TEXT("note10.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 10) {
+		PlaySound(TEXT("note11.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 11) {
+		PlaySound(TEXT("note12.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 12) {
+		PlaySound(TEXT("note13.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 13) {
+		PlaySound(TEXT("note14.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 14) {
+		PlaySound(TEXT("note15.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 15) {
+		PlaySound(TEXT("note16.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 16) {
+		PlaySound(TEXT("note17.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 17) {
+		PlaySound(TEXT("note18.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 18) {
+		PlaySound(TEXT("note19.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 19) {
+		PlaySound(TEXT("note20.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 20) {
+		PlaySound(TEXT("note21.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 21) {
+		PlaySound(TEXT("note22.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 22) {
+		PlaySound(TEXT("note23.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 23) {
+		PlaySound(TEXT("note24.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 24) {
+		PlaySound(TEXT("note25.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 25) {
+		PlaySound(TEXT("note26.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 26) {
+		PlaySound(TEXT("note27.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 27) {
+		PlaySound(TEXT("note28.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 28) {
+		PlaySound(TEXT("note29.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 29) {
+		PlaySound(TEXT("note30.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 30) {
+		PlaySound(TEXT("note31.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 31) {
+		PlaySound(TEXT("note32.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 32) {
+		PlaySound(TEXT("note33.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 33) {
+		PlaySound(TEXT("note34.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 34) {
+		PlaySound(TEXT("note35.wav"), NULL, SND_FILENAME);
+	}
+	else if (note == 35) {
+		PlaySound(TEXT("note1.wav"), NULL, SND_FILENAME);
+	}
 }

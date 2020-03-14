3c4e6bda0c8416d6d53b6747f688ea4f50139b94
salamander77712 <56451516+salamander77712@users.noreply.github.com>
2020-03-14 13:30:19 -0500

    Update musicai.h

--- musicai.h
+++ musicai.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <Windows.h>
+#include <MMSystem.h>
+#pragma comment(lib, "winmm.lib")
 class Music {
 	std::vector<std::vector<int>> music;
 	std::vector<bool> result;
@@ -15,4 +18,5 @@ public:
 	void takeFeedback(bool good);
 	int makeNote(int position);
 	std::vector<int> makeSong();
+	void playNote(int note);
 };
